cb5ec178e6e13ff42deec2a9f8e24acd9bc79b20
Raphaël <razambettapro@gmail.com>
2026-07-31 13:02:50 +0200

    Première vraie route

---
+++ server/routes/product.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { Product } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des produits." });
+  }
+});
+
+module.exports = router;
